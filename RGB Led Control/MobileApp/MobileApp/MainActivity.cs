1526809cbbe1bf988f5af65f245e71c2b8b52685
Paul Camerloher <paul.camerloher@gmail.com>
2016-10-12 15:24:26 +0200

    Mobile App

---
+++ RGB Led Control/MobileApp/MobileApp/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+
+namespace MobileApp
+{
+    [Activity(Label = "MobileApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private SeekBar sekRed;
+        private SeekBar sekGreen;
+        private SeekBar sekBlue;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            sekRed = FindViewById<SeekBar>("skbRed");
+            sekGreen = FindViewById<SeekBar>("sekGreen");
+            sekBlue = FindViewById<SeekBar>("sekBlue");
+
+            sekRed.SetOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+            {
+
+                @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch 
+            (SeekBar seekBar)
+            {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged  (SeekBar seekBar, int progress,bool fromUser)
+            {
+                // TODO Auto-generated method stub
+
+
+
+                //Change the int value of the doBrightness from here?
+            }
+        }
+        });
+    }
+       
+
+       
+    }
+}
+
---
+++ RGB Led Control/Server/Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        const int SERVER_PORT = 33123;
+        const int MSG_LENGTH = 3;
+        static SerialPort arduino;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.Ticks);
+            Console.Write("Serieller Port: ");
+            string port = Console.ReadLine();
+            Console.Write("Baudrate: ");
+            int baudrate = Convert.ToInt32(Console.ReadLine());
+
+
+            UdpClient server = new UdpClient(new IPEndPoint(IPAddress.Any, SERVER_PORT));
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            arduino = new SerialPort(port, baudrate);
+            arduino.Open();
+
+            while (true)
+            {
+                try
+                {
+                    byte[] msg = server.Receive(ref sender);
+                    if (msg.Length == MSG_LENGTH)
+                    {
+                        arduino.Write(msg, 0, msg.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
