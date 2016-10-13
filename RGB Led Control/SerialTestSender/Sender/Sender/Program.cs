8e5c279714496246a725ce9235e7042cb58c9332
Paul Camerloher <paul.camerloher@gmail.com>
2016-10-13 21:14:52 +0200

    added some functions

--- RGB Led Control/SerialTestSender/Sender/Sender/Program.cs
+++ RGB Led Control/SerialTestSender/Sender/Sender/Program.cs
@@ -13,13 +13,41 @@ namespace Sender
         {
             SerialPort sp = new SerialPort(Console.ReadLine());
             sp.Open();
+            //while (true)
+            //{
+            //    byte a = Convert.ToByte(Console.ReadLine());
+            //    byte b = Convert.ToByte(Console.ReadLine());
+            //    byte c = Convert.ToByte(Console.ReadLine());
+            //    sp.Write(new byte[] { a, b, c }, 0, 3);
+
+            //}
             while (true)
             {
-                byte a = Convert.ToByte(Console.ReadLine());
-                byte b = Convert.ToByte(Console.ReadLine());
-                byte c = Convert.ToByte(Console.ReadLine());
-                sp.Write(new byte[] { a, b, c }, 0, 3);
-
+                for (int i = 0; i < 256; i++)
+                {
+                    sp.Write(new byte[] {(byte) i, (byte) (255 - i), 0}, 0, 3);
+                    System.Threading.Thread.Sleep(20);
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    sp.Write(new byte[] {(byte) (255 - i), (byte) i, 0}, 0, 3);
+                    System.Threading.Thread.Sleep(20);
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    sp.Write(new byte[] { 0, (byte)(255 - i), (byte) i }, 0, 3);
+                    System.Threading.Thread.Sleep(20);
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    sp.Write(new byte[] { (byte)i, 0, (byte)(255 - i) }, 0, 3);
+                    System.Threading.Thread.Sleep(20);
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    sp.Write(new byte[] { (byte)(255 - i), (byte)i,0 }, 0, 3);
+                    System.Threading.Thread.Sleep(20);
+                }
             }
         }
     }
