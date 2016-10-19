6b48c2f6badaad9bbed24e90afe710d651bfacfc
Paul Camerloher <paul.camerloher@gmail.com>
2016-10-19 21:30:42 +0200

    Created new project about evolution
    Created and also started Project "Evolution"

---
+++ EvolutionTagDerOffenenTuere/Genetic Evolution/NeuralNetwork/NeuralNetwork/Network.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class Network
+    {
+        public InputNodeLayer inputNodeLayer;
+        public OutputNodeLayer outputNodeLayer;
+
+        static Random rnd = new Random();
+
+        public Network(int inputNodesCount, int outputNodesCount)
+        {
+            outputNodeLayer = new OutputNodeLayer(outputNodesCount);
+            inputNodeLayer = new InputNodeLayer(inputNodesCount, outputNodeLayer);
+        }
+
+        public List<double> GetResult(List<double> inputs)
+        {
+            inputNodeLayer.AddInputs(inputs);
+            inputNodeLayer.PushOutputs();
+            return outputNodeLayer.GetOutputs();
+        }
+
+        public static Network CompineNetwork(Network n1, Network n2)
+        {
+            if (n1.inputNodeLayer.NodeCount() == n2.inputNodeLayer.NodeCount() &&
+                n1.outputNodeLayer.NodeCount() == n2.outputNodeLayer.NodeCount())
+            {
+                List<double> allWeights1 = n1.inputNodeLayer.GetAllWeights();
+                List<double> allWeights2 = n2.inputNodeLayer.GetAllWeights();
+
+                int split = rnd.Next(0, allWeights1.Count);
+                for (int i = split; i < allWeights1.Count; i++)
+                {
+                    allWeights2[i] = allWeights1[i];
+                }
+
+                Network n = new Network(n1.inputNodeLayer.NodeCount(), n1.outputNodeLayer.NodeCount());
+                n.inputNodeLayer.SetWeights(allWeights2.ToArray());
+                return n;
+            }
+            return null;
+        }
+    }
+}
---
+++ EvolutionTagDerOffenenTuere/Genetic Evolution/NeuralNetwork/NeuralNetwork/Node.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class Node
+    {
+
+        protected double currentInput;
+
+        public Node()
+        {
+        }
+        public virtual double GetOutput()
+        {
+            return currentInput;
+        }
+
+        public void AddInput(double d)
+        {
+            currentInput += d;
+        }
+
+        public void Reset()
+        {
+            currentInput = 0;
+        }
+        
+    }
+
+    class InputNode : Node
+    {
+        private List<Connection> connections;
+
+        public InputNode(List<Node> targetNodes)
+        {
+            connections = new List<Connection>();
+            foreach (var tn in targetNodes)
+            {
+
+                connections.Add(new Connection(tn));
+            }
+        }
+        public override double GetOutput()
+        {
+            double d = (1 / (1 + Math.Pow(Math.E, -currentInput)));
+            return d;
+        }
+
+        public void PushOutputs()
+        {
+            foreach (Connection c in connections)
+            {
+                c.SendInputToTargetNode(GetOutput());
+            }
+        }
+        public List<double> GetWeights()
+        {
+            List<double> d = new List<double>();
+            foreach (Connection c in connections)
+            {
+                d.Add(c.Weight);
+            }
+            return d;
+        }
+
+        public int ConnectionsCount()
+        {
+            return connections.Count;
+        }
+
+        public void SetWeightOfNode(int index, double weight)
+        {
+            connections[index].Weight = weight;
+        }
+    }
+}
